ec9db03f1631da26f9fbd3889ded00562761c8be
99hsueh <99hhsueh@gmail.com>
2017-02-14 21:09:19 +0000

    update ejs version, added partial + pages, implemented retrieving longURL by adding obj in tempVars

--- express_server.js
+++ express_server.js
@@ -1,25 +1,39 @@
 var express = require("express");
 var app = express();
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 8080;
 
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+//set the view engine to ejs
+app.set('view engine', 'ejs');
+
 //listening to port
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
+
 //root directory, index
 app.get("/", (req, res) => {
-  res.end("Hello!");
+  res.render('pages/index');
 });
 
-//say hi page, hello
-app.get("/hello", (req, res) => {
-  res.end("<html><body>Hello <b>World</b></body></html>\n");
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("pages/urls_index", templateVars);
+});
+
+app.get("/urls/:id", (req, res) => {
+  let templateVars = { shortURL: req.params.id, longURL: urlDatabase[req.params.id]};
+  res.render("pages/urls_show", templateVars);
+});
+
+//about the page
+app.get("/about", (req, res) => {
+  res.render('pages/about');
 });
 
 //json page
