5754d2bd25a2bad67de00a61b37f33b67e29d7e4
99hsueh <99hhsueh@gmail.com>
2017-02-14 22:32:23 +0000

    post to /urls, shortURL(random string text) and longURL(req.params.longURL)

--- express_server.js
+++ express_server.js
@@ -7,6 +7,19 @@ var urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
+function generateRandomString() {
+  var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+    for( var i=0; i < 6; i++ )
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+    return text;
+}
+
 //set the view engine to ejs
 app.set('view engine', 'ejs');
 
@@ -21,20 +34,35 @@ app.get("/", (req, res) => {
   res.render('pages/index');
 });
 
+app.get("/about", (req, res) => {
+  res.render('pages/about');
+});
+
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("pages/urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("pages/urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(generateRandomString(),req.body.longURL);  // debug statement to see POST parameters
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
 app.get("/urls/:id", (req, res) => {
-  let templateVars = { shortURL: req.params.id, longURL: urlDatabase[req.params.id]};
+  //add in object for ejs to access. urlDatabase[req.params.id]
+  //and not urlDatabase[shortURL] because shortURL does not hold the value
+  let templateVars = { shortURL: req.params.id,
+                       longURL: urlDatabase[req.params.id]
+                     };
   res.render("pages/urls_show", templateVars);
 });
 
 //about the page
-app.get("/about", (req, res) => {
-  res.render('pages/about');
-});
+
 
 //json page
 app.get("/urls.json", (req, res) => {
