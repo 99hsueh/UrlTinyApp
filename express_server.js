f75a0d809a45fd0eec8ad9cfc6e4479e28e9b99f
99hsueh <99hhsueh@gmail.com>
2017-02-18 03:18:02 +0000

    completing tech spec. up to get login atm

--- express_server.js
+++ express_server.js
@@ -16,8 +16,11 @@ let urlDatabase = {
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
-const cookieParser = require("cookie-parser");
-app.use(cookieParser());
+const cookieSession = require("cookie-session");
+app.use(cookieSession({
+  name: "session",
+  keys: ["qwertyuiop"]
+}));
 
 function generateRandomString() {
   let text = "";
@@ -40,21 +43,36 @@ app.listen(PORT, () => {
 
 //root directory, index
 app.get("/", (req, res) => {
-  let newKey = req.cookies.user_id;
+  console.log(req.session);
+  let newKey = req.session.user_id;
   let templateVars = {user: users[newKey]};
-  res.render('pages/index', templateVars);
+  if (users[newKey]) {
+    res.redirect('/urls');
+  } else {
+    res.redirect('/login');
+  }
+});
+
+app.get("/login", (req, res) => {
+  let newKey = req.session.user_id;
+  let templateVars = {user: users[newKey]};
+  if (users[newKey]) {
+    res.redirect('/');
+  } else {
+    res.status(200).redirect("/register");
+  }
 });
 
 //about the page
 app.get("/about", (req, res) => {
-  let newKey = req.cookies.user_id;
+  let newKey = req.session.user_id;
   let templateVars = {user: users[newKey]};
   res.render('pages/about', templateVars);
 });
 
 //list urls
 app.get("/urls", (req, res) => {
-  let newKey = req.cookies.user_id;
+  let newKey = req.session.user_id;
   console.log(newKey);
   if(users[newKey]){
     let listOfURL = [];
@@ -70,20 +88,21 @@ app.get("/urls", (req, res) => {
     };
       console.log(templateVars);
 
-    res.render("pages/urls_index", templateVars);
+    res.status(200).render("pages/urls_index", templateVars);
   } else {
-    res.status(403).send("please login")
+    let templateVars = {user: users[newKey]};
+    res.status(401).render('pages/error401', templateVars);
   }
 });
 
 //create new url
 app.get("/urls/new", (req, res) => {
-  let newKey = req.cookies.user_id;
+  let newKey = req.session.user_id;
   let templateVars = {user: users[newKey]};
   if(users[newKey]){
-    res.render("pages/urls_new", templateVars);
+    res.status(200).render("pages/urls_new", templateVars);
   } else {
-    res.redirect("/");
+    res.status(401).render('pages/error401', templateVars);
   }
 });
 
@@ -91,14 +110,25 @@ app.get("/urls/new", (req, res) => {
 app.get("/urls/:id", (req, res) => {
   //add in object for ejs to access. urlDatabase[req.params.id]
   //and not urlDatabase[shortURL] because shortURL does not hold the value
-  let newKey = req.cookies.user_id;
+  let newKey = req.session.user_id;
   let templateVars = {
     shortURL: req.params.id,
     longURL: urlDatabase[req.params.id].longURL,
     user: users[newKey],
     urls: urlDatabase
   };
+  if(req.params.id != urlDatabase[req.params.id].id){
+    res.status(404).render('pages/error404', templateVars);
+  }
+  else if(!users[newKey]){
+    res.status(401).render('pages/error401', templateVars);
+  }
+  else if(newKey != urlDatabase[req.params.id].user_id){
+    res.status(403).render('pages/error403', templateVars);
+  }
+  else {
   res.render("pages/urls_show", templateVars);
+  }
 });
 
 app.get("/u/:shortURL", (req, res) => {
@@ -112,7 +142,7 @@ app.get("/u/:shortURL", (req, res) => {
 });
 
 app.get("/register", (req, res) => {
-  let newKey = req.cookies.user_id;
+  let newKey = req.session.user_id;
   let templateVars = {user: users[newKey]};
   res.render("pages/register", templateVars);
 })
@@ -126,14 +156,14 @@ app.get("/urls.json", (req, res) => {
 //but redirect to /urls/"shortURL"
 app.post("/urls", (req, res) => {
   let shortURL = generateRandomString();
-  let newKey = req.cookies.user_id;
+  let newKey = req.session.user_id;
   urlDatabase[shortURL] = {id:shortURL, longURL: req.body.longURL,user_id:users[newKey].user_id};
   res.redirect("/urls/" + shortURL);
 });
 
 //update url's long url
 app.post("/urls/:id", (req, res) => {
-  let newKey = req.cookies.user_id;
+  let newKey = req.session.user_id;
   let shortURL = req.params.id
   let longURL = req.body.longURL;
   if(shortURL.user_id === newKey){
@@ -153,6 +183,8 @@ app.post("/urls/:id/delete", (req, res) => {
 
 app.post("/login", (req, res) => {
   let user;
+  let newKey = req.session.user_id;
+  let templateVars = {user: users[newKey]};
   for (let IdKey in users) {
     if (users[IdKey].email === req.body.email) {
       user = users[IdKey];
@@ -161,18 +193,17 @@ app.post("/login", (req, res) => {
   }
   if (user){
     if (bcrypt.compareSync(req.body.password, user.password)){
-      res.cookie("user_id", user.user_id);
-      res.redirect('/');
-      console.log(users);
-      return;
+      req.session.user_id = user.user_id;
+      res.redirect('/urls');
+    } else {
+      res.status(401).render('pages/error401', templateVars);
     }
-  } else{
-  res.status(401).send('Bad credentials');
+  } else {res.status(401).render('pages/error401', templateVars);
   }
 });
 
 app.post("/logout", (req, res) => {
-  res.clearCookie("user_id");
+  req.session = null;
   res.redirect("/");
 })
 
@@ -182,13 +213,14 @@ app.post("/register", (req, res) => {
   } else if (!(req.body.email)) {
     res.status(400).send("Your email/password is empty.");
   } else {
-    const newKey = generateRandomString();
+    let newKey = generateRandomString();
     users[newKey] = {
       user_id: newKey,
       email: req.body.email,
       password: bcrypt.hashSync(req.body.password, 10)
     };
-    res.cookie("user_id", newKey)
+    req.session.user_id = newKey;
     res.redirect("/");
+    return;
   }
 })
